4ce6ad4d3056fa88ba730a33f3f5e0eda4acd7be
Junior Developer <lotifurshabbir54@gmail.com>
2023-04-10 16:37:55 +0600

    bootcamp sent

---
+++ C-Bootchamp/c bootcamp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    //code starts here.....
+    int a;
+    scanf("%d",&a);
+    if(a<=300)
+    {
+        printf("%d",a*365);
+    }
+    return 0;
+}
